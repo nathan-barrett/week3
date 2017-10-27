398fead52f859fa8b0ffc868421a2516d4a9266e
Nathan Barrett <demeules.barrett@gmail.com>
2017-10-27 13:57:21 -0700

    started backend logic over

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 //business logic//
+var zero = /[0]/;
+    one = /[1]/;
+    oneZero = /[01]/;
 var inputArray = [];
-var counter = function(input) {
-    for (var i = 0; i <=input; i++) {
-      inputArray.push(i);
-      console.log(inputArray);
-    }
-    for (var arrayIndex = 0; arrayIndex < inputArray; arrayIndex ++) {
+
+
+var numTest = function(input) {
+  if ((parseInt(number) % 3 === 0) && (parseInt(number) !== 0)) {
+    inputArray.push(" I'm sorry Dave, I'm afraid I can't do that");
+  } else if (!oneZero.test(input)) {
+    return number;
+  } else if (!zeroRegex.test(number)) {
+    return "Boop"
+  } else if (!oneRegex.test(number)){
+    return "Beep";
+  } else {
+    return "Beep-Boop"
+  };
+};
+
       } if (inputArray[arrayIndex] == 0) {
         inputArray[arrayIndex] = "Beep";
       } else if (inputArray[arrayIndex] == 1) {
