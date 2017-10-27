63913c8280f2fdf4967801fac8a51c45495dbf85
nathan barrett <demeules.barrett@gmail.com>
2017-10-27 09:18:37 -0700

    passed first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 //business logic//
+
+var Boop = /1/;
+var Beep = /0/;
+var output = [];
+
+var counter = function(userInput) {
+  for (var i =1; i <= userInput; i++) {
+    output.push(i);
+  }
+  alert(output);
+
+};
+
+//UI Logic//
+$(document).ready(function(){
+  $("form#count-form").submit(function(event){
+    event.preventDefault();
+    var countTo = parseInt($("input#count-to").val());
+    counter(countTo);
+  });
+});
