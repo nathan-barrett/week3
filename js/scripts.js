89cd12344e901e954053b64e8303a70e7281c03c
Nathan Barrett <demeules.barrett@gmail.com>
2017-10-27 14:40:51 -0700

    fixed issues by using regex

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,47 @@ var zero = /[0]/;
     one = /[1]/;
     oneZero = /[01]/;
 var inputArray = [];
-
-
-var countToNumber = function(inputNumValue) {
-  var countArray = [];
-  for (i = 0; i <= inputNumValue; i++) {
-    countArray.push(i.toString());
+var counter = function(inputNumValue) {
+  var userArray = [];
+  for (index = 1; index <= inputNumValue; index++) {
+    userArray.push(index.toString());
   }
-  return countArray;
+  return userArray;
 };
-
+var outputToString = function(numArray) {
+  var outputString = numArray.map(function(number) {
+    return numTest(number);
+  });
+  return outputString.join(", ");
+};
 var numTest = function(input) {
-  if ((parseInt(number) % 3 === 0) && (parseInt(number) !== 0)) {
-    inputArray.push(" I'm sorry Dave, I'm afraid I can't do that");
+  if ((parseInt(input) % 3 === 0) && (parseInt(input) !== 0)) {
+    return " I'm sorry Dave, I'm afraid I can't do that"
   } else if (!oneZero.test(input)) {
     return input;
-  } else if (!zero.test(number)) {
+  } else if (!zero.test(input)) {
     return "Boop"
-  } else if (!one.test(number)){
+  } else if (!one.test(input)){
     return "Beep";
   } else {
     return "Beep-Boop"
   }
 }
-var output = function(userInput) {
+var finalOutput = function(userInput) {
   var inputArray = userInput.split("");
-    return outputStringify(numTest(parseInt(userInput)));
+    return outputToString(counter(parseInt(userInput)));
   };
-
-    console.log(inputArray);
-  }
 
 //UI Logic//
 $(document).ready(function(){
   $("form#count-form").submit(function(event){
     event.preventDefault();
-    var countTo = parseInt($("input#count-to").val());
-    var result = counter(countTo);
-    alert(result);
+    var countTo = $("input#count-to").val();
+    var results = finalOutput(countTo);
+    $("#Beep-Boop").empty();
+    resultArray = results.split();
+    resultArray.forEach(function(result){
+      $("#Beep-Boop").append("<li>" + result + "</li>")
+    })
   });
 });
