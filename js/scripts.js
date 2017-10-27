656e4f28e59c10ea16a00541b6e924e4eb082467
nathan barrett <demeules.barrett@gmail.com>
2017-10-27 10:14:49 -0700

    broke script for a hot second

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,35 @@
 //business logic//
 
-var Boop = /1/;
-var Beep = /0/;
-var output = [];
 
-var counter = function(userInput) {
-  for (var i =1; i <= userInput; i++) {
-    output.push(i);
-  }
-  alert(output);
 
+
+
+var counter = function(input) {
+  var inputArray = input.split();
+  var beepBoop = [0, 1]
+  var output = [];
+  for (var i =1; i <= inputArray; i++) {
+    for (var b=0; b < beepBoop.length; i++){
+      debugger
+      if ((i % 3) === 0){
+        output.push(" I'm sorry Dave, I'm afraid I can't do that")
+      } else if (inputArray[i] === beepBoop[0]) {
+        output.push("Beep")
+      } else if (inputArray[i] === beepBoop[1]) {
+        output.push("Boop");
+      } else {
+        output.push(i);
+      }
+    }
+    alert(output);
+  };
 };
 
 //UI Logic//
 $(document).ready(function(){
   $("form#count-form").submit(function(event){
     event.preventDefault();
-    var countTo = parseInt($("input#count-to").val());
+    var countTo = $("input#count-to").val();
     counter(countTo);
   });
 });
