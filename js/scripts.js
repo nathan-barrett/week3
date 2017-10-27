5294646ad7d5fb43ff6eb621205816a9672bd0b3
nathan barrett <demeules.barrett@gmail.com>
2017-10-27 13:39:18 -0700

    messed everything up

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,28 @@
 //business logic//
-
-
-
-
-
+var inputArray = [];
 var counter = function(input) {
-  var inputArray = [];
-  // var beepBoop = [0, 1]
-  for (var i = 0; i <= input; i++) {
-    inputArray.push(i);
+    for (var i = 0; i <=input; i++) {
+      inputArray.push(i);
+      console.log(inputArray);
+    }
+    for (var arrayIndex = 0; arrayIndex < inputArray; arrayIndex ++) {
+      } if (inputArray[arrayIndex] == 0) {
+        inputArray[arrayIndex] = "Beep";
+      } else if (inputArray[arrayIndex] == 1) {
+        inputArray[arrayIndex] = "Boop";
+      } else if ((inputArray[arrayIndex] % 3) === 0){
+        inputArray[arrayIndex] = " I'm sorry Dave, I'm afraid I can't do that";
+    }
+    return inputArray;
+    console.log(inputArray);
   }
-
-  for (var index = 0; index < inputArray.length; index += 1) {
-    if ((index % 3) === 0){
-    // inputArray[index] = " I'm sorry Dave, I'm afraid I can't do that";
-    index = " I'm sorry Dave, I'm afraid I can't do that");
-    } else if (index == beepBoop[0]) {
-        // inputArray[i] = "Boop";
-        output.push("Boop");
-      } else if (index == beepBoop[1]) {
-        // inputArray[i] = "Beep";
-        output.push("Beep");
-      } else {
-        output.push(index);
-      }
-      console.log(output);
-    };
-  };
-};
 
 //UI Logic//
 $(document).ready(function(){
   $("form#count-form").submit(function(event){
     event.preventDefault();
-    var countTo = $("input#count-to").val();
-    counter(countTo);
+    var countTo = parseInt($("input#count-to").val());
+    var result = counter(countTo);
+    alert(result);
   });
 });
